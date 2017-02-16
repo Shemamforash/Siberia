73abaa09e38f31f84819492e70e1d81b69d25d6f
Shemamforash <samwest2@hotmai.co.uk>
2017-02-16 13:31:02 +0000

    Projectiles Work

--- Siberia/Assets/Scripts/Player.cs
+++ Siberia/Assets/Scripts/Player.cs
@@ -6,26 +6,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float move_speed = 0.2f;
-    public float x_max_bound = 1, x_min_bound = -1, y_max_bound = 1, y_min_bound = -1;
+    public float move_speed = 1f;
+    public float x_max_bound = 2, x_min_bound = -2, y_max_bound = 2, y_min_bound = -2;
 
     public GameObject beam_prefab, projectile_prefab;
-    private enum states {dark, light};
+    private enum states { dark, light };
     private states current_state;
+    private bool fired_projectile = false;
 
-    private void TakeMouse() {
-        if(Input.GetMouseButton(0)){
-            if(current_state == states.dark) {
+    private void TakeMouse()
+    {
+        if (Input.GetMouseButton(0) && fired_projectile == false)
+        {
+            if (current_state == states.dark)
+            {
                 GameObject.Instantiate(projectile_prefab, transform.position, transform.rotation);
-            } else {
+                fired_projectile = true;
+            }
+            else
+            {
 
             }
+        }
+        if(Input.GetMouseButtonUp(0)){
+            fired_projectile = false;
         }
     }
 
     private void ClampToBounds()
     {
-		Vector3 new_position = transform.position;
+        Vector3 new_position = transform.position;
         //Y bounds
         if (transform.position.y < y_min_bound)
         {
@@ -53,13 +63,14 @@ public class Player : MonoBehaviour
         Vector3 reference_dir = new Vector3(0, 1, 0);
         Vector3 mouse_screen = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 mouse_position = mouse_screen;
-		mouse_position = mouse_position - transform.position;
+        mouse_position = mouse_position - transform.position;
         mouse_position.z = 0;
         float dot_product = Vector3.Dot(reference_dir, mouse_position);
         float theta = Mathf.Acos(dot_product / mouse_position.magnitude) * Mathf.Rad2Deg;
-		if(mouse_screen.x < transform.position.x){
-			theta = 360 - theta;
-		}
+        if (mouse_screen.x < transform.position.x)
+        {
+            theta = 360 - theta;
+        }
         Vector3 vect_rotation = new Vector3(0, 0, 360 - theta);
         Quaternion new_rotation = Quaternion.Euler(vect_rotation);
         transform.rotation = new_rotation;
@@ -84,14 +95,14 @@ public class Player : MonoBehaviour
         {
             movement_difference.x += move_speed;
         }
-        transform.position += movement_difference;
+        transform.position += movement_difference * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-		TakeInput();
-		ClampToBounds();
+        TakeInput();
+        ClampToBounds();
         PointToMouse();
         TakeMouse();
     }
---
+++ Siberia/Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    private float time_alive = 0f;
+	public float duration = 3f, speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * speed;
+		time_alive += Time.deltaTime;
+		if(time_alive > duration){
+			Destroy(gameObject);
+		}
+    }
+}
