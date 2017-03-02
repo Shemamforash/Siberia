0faf37a4f85567b561a3ac412fab606348d56ce0
Mattadon <ml2g14@soton.ac.uk>
2017-03-02 20:04:08 +0000

    Added door to tutorial that opens if dialog option chosen

---
+++ Siberia/Assets/Scripts/DialogDoorController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogDoorController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject skipTutorialDialog;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            skipTutorialDialog.SetActive(true);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        Destroy(gameObject);
+    }
+}
--- Siberia/Assets/Scripts/TutorialDoorController.cs
+++ Siberia/Assets/Scripts/TutorialDoorController.cs
@@ -7,17 +7,6 @@ public class TutorialDoorController : MonoBehaviour
 
     private int enemies_left = 0;
 
-	// Use this for initialization
-	void Start ()
-    {
-        //enemies_left = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log("Enemies on door: " + enemies_left);
-	}
-
     public void addEnemy()
     {
         enemies_left++;
