a61fd33e1177bbafd82026ef9defdf54fc8c5f84
Matthew Langford <ml2g14@soton.ac.uk>
2017-03-03 19:16:20 +0000

    Added icons to tutorial that bring up info dialogs

---
+++ Siberia/Assets/Scripts/TutorialInfoIcon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialInfoIcon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tutorialBox1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        tutorialBox1.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        tutorialBox1.SetActive(false);
+    }
+}
