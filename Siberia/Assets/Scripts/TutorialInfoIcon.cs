9893a3b2268a7a523e2b38a4d3f50ef80b514fb5
Mattadon <ml2g14@soton.ac.uk>
2017-03-03 20:58:39 +0000

    Added all info dialogs to tutorial level

--- Siberia/Assets/Scripts/TutorialInfoIcon.cs
+++ Siberia/Assets/Scripts/TutorialInfoIcon.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Attach to rollover icons to display information dialogs that provide helpful (?) tips
+ */
 public class TutorialInfoIcon : MonoBehaviour
 {
     [SerializeField]
     private GameObject tutorialBox1;
+    [SerializeField]
+    private GameObject tutorialBox2;
+    [SerializeField]
+    private GameObject tutorialBox3;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        tutorialBox1.SetActive(true);
+        if(tutorialBox1 != null)
+            tutorialBox1.SetActive(true);
+
+        if(tutorialBox2 != null)
+            tutorialBox2.SetActive(true);
+
+        if (tutorialBox3 != null)
+            tutorialBox3.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        tutorialBox1.SetActive(false);
+        if(tutorialBox1 != null)
+            tutorialBox1.SetActive(false);
+
+        if(tutorialBox2 != null)
+            tutorialBox2.SetActive(false);
+
+        if (tutorialBox3 != null)
+            tutorialBox3.SetActive(false);
     }
 }
